22cfd9f5731a93cd103ecc6e6f0a8bb8d47f9a21
lndebele115-ui <lndebele115@gmail.com>
2025-08-17 11:16:19 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;vFMTkDrL#NRUVuUgWx9qYitceCB5z9mNF4ZuzK5JlOcRj4qDEu3o", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "27660899221", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
